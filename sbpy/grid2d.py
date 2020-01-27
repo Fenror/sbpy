dc50099fca0db51d8ff286276d434f0fea8b30d5
Oskar <oskar.alund@gmail.com>
2020-01-27 20:30:44 +0100

    Work on Multiblock

--- sbpy/grid2d.py
+++ sbpy/grid2d.py
@@ -7,7 +7,21 @@ rc('text', usetex=True)
 
 
 class Multiblock:
-    """ Represents a structured multiblock grid. """
+    """ Represents a structured multiblock grid.
+
+    Attributes:
+        X_blocks: A list of 2D numpy arrays containing x-values for each
+                  block.
+        Y_blocks: A list of 2D numpy arrays containing y-values for each
+                  block.
+        corners: A list of unique corners in the grid.
+        faces: A list of index-quadruples defining the blocks. For example, if
+               faces[n] = [i,j,k,l], and (X,Y) are the matrices corresponding to
+               block n, then (X[0,0],Y[0,0]) = corners[i]
+                             (X[-1,0],Y[-1,0]) = corners[j]
+                             (X[-1,-1],Y[-1,-1]) = corners[k]
+                             (X[0,-1],Y[0,-1]) = corners[l]
+    """
 
     def __init__(self, X_blocks, Y_blocks):
         """ Initializes a Multiblock object.
@@ -25,6 +39,24 @@ class Multiblock:
 
         self.X_blocks = X_blocks
         self.Y_blocks = Y_blocks
+
+        # Save unique corners
+        self.corners = []
+        for X,Y in zip(X_blocks, Y_blocks):
+            self.corners.append(self.get_corners(X,Y))
+
+        self.corners = np.unique(np.concatenate(self.corners), axis=0)
+
+        # Save faces in terms of unique corners
+        self.faces = []
+
+        for k,(X,Y) in enumerate(zip(X_blocks, Y_blocks)):
+            block_corners = self.get_corners(X,Y)
+            indices = []
+            for c in block_corners:
+                idx = np.argwhere(np.all(c == self.corners, axis=1)).item()
+                indices.append(idx)
+            self.faces.append(np.array(indices))
 
 
     def plot_grid(self):
@@ -82,7 +114,10 @@ class Multiblock:
 
 
     def get_corners(self,X,Y):
-        """ Returns the corners of a block. """
+        """ Returns the corners of a block.
+
+        Starts with (X[0,0], Y[0,0]) and continues counter-clockwise.
+        """
         return np.array([[X[0,0]  , Y[0,0]  ],
                          [X[-1,0] , Y[-1,0] ],
                          [X[-1,-1], Y[-1,-1]],
@@ -93,9 +128,6 @@ class Multiblock:
         """ Returns the center point of a block. """
         corners = self.get_corners(X,Y)
         return 0.25*(corners[0] + corners[1] + corners[2] + corners[3])
-
-
-
 
 
 def load_p3d(filename):
