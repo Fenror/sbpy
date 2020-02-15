7df5821c78c817343fa05bee6284b613b66463f1
Oskar <oskar.alund@gmail.com>
2020-02-15 19:46:32 +0100

    Remove some comments

--- sbpy/multiblock_solvers.py
+++ sbpy/multiblock_solvers.py
@@ -356,10 +356,6 @@ class AdvectionDiffusionSolver:
             vx = self.Ux[idx2][bd_slice2]
             vy = self.Uy[idx2][bd_slice2]
             normals = self.grid.get_normals(idx1, side1)
-            #fluxu = np.array([ux*n1 + uy*n2 for (ux,uy,(n1,n2)) in
-            #                 zip(ux,uy,normals)])
-            #fluxv = np.array([vx*n1 + vy*n2 for (vx,vy,(n1,n2)) in
-            #                 zip(vx,vy,normals)])
             fluxu = normals[:,0]*ux + normals[:,1]*uy
             fluxv = normals[:,0]*vx + normals[:,1]*vy
 
@@ -388,8 +384,6 @@ class AdvectionDiffusionSolver:
             uy = self.Uy[block_idx][bd_slice]
             normals = self.grid.get_normals(block_idx, side)
             flow_vel = self.flow_velocity[block_idx][side]
-            #flux = np.array([ux*n1 + uy*n2 for (ux,uy,(n1,n2)) in
-                             #zip(ux,uy,normals)])
             flux = normals[:,0]*ux + normals[:,1]*uy
 
             out_bc = self.eps*flux
